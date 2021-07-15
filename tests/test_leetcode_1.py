c48089ef5f861843c8aafe20523ca47c4ad20281
jitechpc3 <you@example.com>
2021-07-15 17:34:05 +0400

    append_type_hinter

--- tests/test_leetcode_1.py
+++ tests/test_leetcode_1.py
@@ -8,13 +8,13 @@ from leetcode.leetcode_1 import title_to_number
 
 @pytest.mark.parametrize("columntitle, result", [("A", 1), ('AB', 28),
                          ('ZY', 701), ('FXSHRXW', 2147483647)])
-def test_1(columntitle, result):
+def test_1(columntitle: str, result: int):
     '''test_leetcode_1'''
     assert title_to_number(columntitle) == result
 
 
 @pytest.mark.parametrize("columntitle, result", [("ZY", 50),
                          ("C", 2), ('A', 5)])
-def test_1_negative(columntitle, result):
+def test_1_negative(columntitle: str, result: int):
     '''negative_test_leetcode_1'''
     assert title_to_number(columntitle) != result
--- tests/test_leetcode_3.py
+++ tests/test_leetcode_3.py
@@ -12,13 +12,13 @@ from leetcode.leetcode_3 import two_sum
 
 @pytest.mark.parametrize("nums, target, result", [("2 7 11 15", 9, [0, 1]),
                          ('3 2 4', 6, [1, 2]), ('3 3', 6, [0, 1])])
-def test_3(nums, target, result):
+def test_3(nums: str, target: int, result: list):
     '''test_leetcode_3'''
     assert two_sum(nums.split(), target) == result
 
 
 @pytest.mark.parametrize("nums, target, result", [("8 5 9", 9, [0]),
                          ('3 2 4', 15, [1, 2]), ('3 3', 6, [0, 0])])
-def test_3_negative(nums, target, result):
+def test_3_negative(nums: str, target: int, result: list):
     '''negative_test_leetcode_3'''
     assert two_sum(nums.split(), target) != result
