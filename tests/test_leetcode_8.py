1e84971dc858c99f956ae3b602a7f01da1216952
jitechpc3 <you@example.com>
2021-07-19 10:00:13 +0400

    adding_type-hinter_to_test_leetcode_8.py_file_for_partition_labels_function

--- tests/test_leetcode_8.py
+++ tests/test_leetcode_8.py
@@ -2,7 +2,6 @@
 You are given a string s. We want to partition the string into as many
 parts as possible so that each letter appears in at most one part.
 Return a list of integers representing the size of these parts.'''
-
 import pytest
 
 from leetcode.leetcode_8 import partition_labels
