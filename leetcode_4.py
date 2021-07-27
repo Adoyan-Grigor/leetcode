897e37c466c577e8edc29435a213da1fdcf04a70
jitechpc3 <you@example.com>
2021-07-27 16:49:13 +0400

    adding type-hinter to leetcode_4.py file for int_toroman function

--- leetcode_4.py
+++ leetcode_4.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-'''leetcode_4'''
+'''leetcode_4
+   performs the task by reference
+   (https://leetcode.com/problems/integer-to-roman/)'''
 
 
 def int_toroman(num: int) -> str:
-    '''https://leetcode.com/problems/integer-to-roman/'''
+    '''converts integer num to roman numbers'''
     res = ''
     mapping = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC',
                50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
