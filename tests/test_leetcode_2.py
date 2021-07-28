1cc596d25be7d2b894923575f25b2642d3de3cae
jitechpc3 <you@example.com>
2021-07-28 14:31:40 +0400

    adding type-hinter to test_leetcode_2.py file for is_anagram function

--- tests/test_leetcode_2.py
+++ tests/test_leetcode_2.py
@@ -1,20 +1,21 @@
-'''https://leetcode.com/problems/valid-anagram/
-Given two strings s and t, return true if t is an
-anagram of s, and false otherwise.'''
+'''tests the is_anagram function in leetcode_2.py file
+   Given two strings st_s and st_t, returns true if st_t is
+   an anagram of st_s, and false otherwise.'''
+
 import pytest
 
 from leetcode.leetcode_2 import is_anagram
 
 
-@pytest.mark.parametrize("st_s, st_t, result", [('anagram', 'nagaram', True),
-                         ('rat', 'car', False)])
-def test_2(st_s: str, st_t: str, result: bool):
+@pytest.mark.parametrize("st_s, st_t, result", [('anagram', 'nagaram', 'True'),
+                         ('rat', 'car', 'False'), ('a', 'a', 'True'), ('a', 'b', 'False')])
+def test_2(st_s: str, st_t: str, result: str):
     '''test_leetcode_2'''
     assert is_anagram(st_s, st_t) == result
 
 
-@pytest.mark.parametrize('st_s, st_t, result', [('aasd', 'asd', True),
-                         ('bca', 'abc', False)])
-def test_2_negative(st_s: str, st_t: str, result: bool):
+@pytest.mark.parametrize('st_s, st_t, result', [('aasd', 'asd', 'True'),
+                         ('bca', 'abc', 'False')])
+def test_2_negative(st_s: str, st_t: str, result: str):
     '''negative_test_leetcode_2'''
     assert is_anagram(st_s, st_t) != result
