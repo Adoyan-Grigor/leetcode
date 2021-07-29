4c03a4b4c06e644bb5458e5e4ab25ca8571530cb
jitechpc3 <you@example.com>
2021-07-29 16:32:31 +0400

    adding type-hinter to leetcode_2.py file for is_anagram function

--- tests/test_leetcode_2.py
+++ tests/test_leetcode_2.py
@@ -9,14 +9,14 @@ from leetcode.leetcode_2 import is_anagram
 
 @pytest.mark.parametrize("st_s, st_t, result", [('anagram', 'nagaram', True),
                          ('rat', 'car', False), ('a', 'a', True),
-                         ('a', 'b', False)])
+                         ('a', 'b', False), ('aacc', 'ccac', False)])
 def test_2(st_s: str, st_t: str, result: bool):
     '''test_leetcode_2'''
     assert is_anagram(st_s, st_t) == result
 
 
 @pytest.mark.parametrize('st_s, st_t, result', [('aasd', 'asd', True),
-                         ('bca', 'abc', False)])
+                         ('bca', 'abc', False), ('ddee', 'dded', True)])
 def test_2_negative(st_s: str, st_t: str, result: bool):
     '''negative_test_leetcode_2'''
     assert is_anagram(st_s, st_t) != result
