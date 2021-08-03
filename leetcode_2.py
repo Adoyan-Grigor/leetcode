8a751048b4ac1d5b3e425d586cd6ad26ae8744b4
jitechpc3 <you@example.com>
2021-08-03 12:21:52 +0400

    code rewriting

--- leetcode_2.py
+++ leetcode_2.py
@@ -7,19 +7,20 @@
 def is_anagram(st_s: str, st_t: str) -> bool:
     '''Given two strings st_s and st_t, returns true if st_t is
        an anagram of st_s, and false otherwise.'''
-    st_g = 0
-    st_f = []
-    for st_k in st_t:
-        st_f.append(st_k)
-    for st_i in st_s:
-        if st_i in st_f:
-            st_g += 1
-            st_f.remove(st_i)
-        else:
+    if len(st_s) != len(st_t):
+        res = False
+        return res
+    for i in st_s:
+        s_num = 0
+        t_num = 0
+        for index_s in st_s:
+            if index_s == i:
+                s_num += 1
+        for index_t in st_t:
+            if index_t == i:
+                t_num  += 1
+        if s_num != t_num:
             res = False
             return res
-    if st_g == len(st_s) and len(st_s) == len(st_t):
-        res = True
-        return res
-    res = False
+    res = True
     return res
